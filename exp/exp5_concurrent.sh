d72f38f00ce2f50882f74e9f29e51a30b18ed6c6
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-04-20 15:05:49 +0000

    [core] exp5_concurrent: fix exp settings
        + remove outdated bindings
        + remove unused workloads
        + fix exp order for insert-only and update-only workloads
    
        TODO: use -preload for volatile variant instead of read-only cache

--- exp/exp5_concurrent.sh
+++ exp/exp5_concurrent.sh
@@ -13,13 +13,13 @@ ISPN_DFLT_CFG="${YCSB_DIR}/infinispan/src/main/conf/infinispan-config.xml"
 ISPN_JNVM_CFG="${YCSB_DIR}/infinispan/src/main/conf/infinispan-jnvm-config.xml"
 
 bindings="infinispan infinispan-jnvm"
-#bindings="jnvm-vmap jnvm-rmap infinispan"
 recordcounts="1000000"
 minoperationcount=10000000
 fieldcounts="10"
-workloads="insertw workloada updatew workloadc"
+workloads="workloada workloadc"
+#workloads="workloada updatew workloadc insertw"
 distribution="zipfian"
-threads="1 "`seq 4 4 40`
+threads="1 2 "`seq 4 4 40`
 ycsb_jobs="run"
 dataintegrity="true"
 
@@ -51,6 +51,7 @@ for binding in $bindings ; do
       loadcachesize=$(( $recordcount * $loadcacheproportion / 100 ))
     fi
 
+    for thread in $threads ; do
     rm -fr /pmem{0,1,2,3}/*
     sed -e 's/size=\".*\"/size=\"'"${loadcachesize}"'\"/g' -i $ISPN_CFG
     sed -e 's/read-only=\"true\"/read-only=\"'"${defaultreadonly}"'\"/g' \
@@ -62,17 +63,18 @@ for binding in $bindings ; do
       -p operationcount=$operationcount\
       -p fieldcount=$fieldcount\
       -p requestdistribution=$distribution\
-      -p measurementtype=hdrhistogram
       -p measurementtype=hdrhistogram\
       -p hdrhistogram.output.path=$LOGDIR/$binding.load.workloada."true".$recordcount.$loadcachesize.$fieldcount.$distribution.1.hdr.log\
       >> $LOGDIR/$binding.load.workloada."true".$recordcount.$loadcachesize.$fieldcount.$distribution.1.log
+    for workload in $workloads ; do
+      [ $workload == "insertw" ] && operationcount=$recordcount && rm -fr /pmem{0,1,2,3}/*
+      [ $workload != "insertw" ] && operationcount=$minoperationcount
     for cachesize in $cachesizes ; do
       [ $(( $cachesize / $recordcount )) -eq 1 ] && readonly="true" || readonly=$defaultreadonly
+      [ $(( $cachesize / $recordcount )) -eq 1 ] && operationcount="30000000"
       sed -e 's/size=\".*\"/size=\"'"${cachesize}"'\"/g' -i $ISPN_CFG
       sed -e 's/read-only=\"true\"/read-only=\"'"${readonly}"'\"/g' \
           -e 's/read-only=\"false\"/read-only=\"'"${readonly}"'\"/g' -i $ISPN_CFG
-    for thread in $threads ; do
-      for workload in $workloads ; do
         for integrity in $dataintegrity ; do
           for ycsb_job in $ycsb_jobs ; do
             $PIN_CPU ${YCSB_DIR}/bin/ycsb.sh $ycsb_job $binding -P ${YCSB_DIR}/workloads/$workload -threads $thread\
