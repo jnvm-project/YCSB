b17c4a205af0becba4e5d2f5c524b23f008c383e
Anatole Lefort <anatole.lefort@telecom-sudparis.eu>
2021-04-26 12:59:43 +0000

    [core] log_to_data: more columns (a lot more)

--- exp/log_to_data.sh
+++ exp/log_to_data.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-experiments="exp0.exectime exp1.cachesize exp2.keycount exp3.distribution exp4.objsize exp5.concurrent exp6.fieldcount"
+#experiments="exp0.exectime exp1.cachesize exp2.keycount exp3.distribution exp4.objsize exp5.concurrent exp6.fieldcount exp7.marshalling"
+experiments="exp00.heapsize"
 
 for experiment in $experiments ; do
 
@@ -19,9 +20,19 @@ for logfilepath in $LOGDIR/*.log ; do
     recordcount=`echo $logfile | cut -d "." -f5`
     cachesize=`echo $logfile | cut -d "." -f6`
     fieldcount=`echo $logfile | cut -d "." -f7`
-    distribution=`echo $logfile | cut -d "." -f8`
-    threads=`echo $logfile | cut -d "." -f9`
-    operationcount=`sed -e 's/.*\ operationcount\=\(.*\)\ -p\ fieldcount.*/\1/gp' $logfilepath | head -n 1`
+    fieldlength=`echo $logfile | cut -d "." -f8`
+    distribution=`echo $logfile | cut -d "." -f9`
+    threads=`echo $logfile | cut -d "." -f10`
+    readonly=`echo $logfile | cut -d "." -f11`
+    preload=`echo $logfile | cut -d "." -f12`
+    memory=`echo $logfile | cut -d "." -f13`
+    oop=`echo $logfile | cut -d "." -f14`
+    filesystem=`echo $logfile | cut -d "." -f15`
+    n_run=`echo $logfile | cut -d "." -f16`
+    operationcount=`sed -n -e 's/.*\ operationcount\=\(.*\)\ -p\ fieldcount.*/\1/gp' $logfilepath`
+    heapsize=`sed -n -e 's/.*\ -Xmx\(.*\)\ -XX:+UseG1GC.*/\1/gp' $logfilepath | sed 's/g/GB/'`
+    hinitoccper=`sed -n -e 's/.*\ -XX:InitiatingHeapOccupancyPercent=\(.*\)\ -classpath.*/\1/gp' $logfilepath`
+    [ -z $hinitoccper ] && hinitoccper="40"
 
     while IFS=", " read col1 col2 col3 ; do
         operation=""
@@ -43,11 +54,15 @@ for logfilepath in $LOGDIR/*.log ; do
                         operation=`echo ${col1:1:-1} | tr '[:upper:]' '[:lower:]'`
                         value="$col3"
                         ;;
+                    *"Time(ms)"*)
+                        operation=`echo ${col1:1:-1} | sed 's/\%/ms/' | tr '[:upper:]' '[:lower:]'`
+                        value="$col3"
+                        ;;
                     *)
                         ;;
                 esac
                 ;;
-            "[INIT]"|"[CLEANUP]"|"[INSERT]"|"[READ]"|"[UPDATE]"|"[VERIFY]")
+            "[INSERT]"|"[READ]"|"[UPDATE]"|"[READ-MODIFY-WRITE]"|"[VERIFY]")
                 case $col2 in
                     *"AverageLatency"*)
                         operation=`echo ${col1:1:-1} | tr '[:upper:]' '[:lower:]'`
@@ -57,10 +72,30 @@ for logfilepath in $LOGDIR/*.log ; do
                         ;;
                 esac
                 ;;
-            "[LOAD]"|"[TRANSACTION]")
+            "[LOAD]"|"[TRANSACTION]"|"[INIT]"|"[CLEANUP]")
                 case $col2 in
                     *"MaxLatency"*)
                         operation=`echo ${col1:1:-1} | tr '[:upper:]' '[:lower:]'`
+                        value="$col3"
+                        ;;
+                    *)
+                        ;;
+                esac
+                ;;
+            "[LOAD_GCs_"*"]"|"[MAIN_GCs_"*"]")
+                case $col2 in
+                    *"MaxLatency"*)
+                        operation=`echo ${col1:1:-1} | sed -e 's/GCs/GC/' -e 's/(ms)/_ms/' | tr '[:upper:]' '[:lower:]'`
+                        value="$col3"
+                        ;;
+                    *)
+                        ;;
+                esac
+                ;;
+            "[USED_MEM_MB]")
+                case $col2 in
+                    *"MinLatency"*|*"MaxLatency"*)
+                        operation=`echo ${col1:1:-1}_${col2:0:3} | tr '[:upper:]' '[:lower:]'`
                         value="$col3"
                         ;;
                     *)
@@ -74,13 +109,9 @@ for logfilepath in $LOGDIR/*.log ; do
         [ -z $operation ] && continue
         [ -z $value ] && continue
 
-        #datafile="$binding.$workload.$ycsb_job.$integrity.$distribution.$operation.dat"
-        #dataheader="#cachesize,recordcount,fieldcount,threads,value"
-        #dataline="$cachesize,$recordcount,$fieldcount,$threads,$value"
-
         datafile="$experiment.dat"
-        dataheader="#cachesize,recordcount,fieldcount,operationcount,threads,binding,workload,integrity,distribution,operation,value"
-        dataline="$cachesize,$recordcount,$fieldcount,$operationcount,$threads,$binding,${workload}_${ycsb_job},$integrity,$distribution,$operation,$value"
+        dataheader="#cachesize,recordcount,fieldcount,fieldlength,operationcount,threads,binding,workload,integrity,distribution,readonly,preload,memory,oop,filesystem,heapsize,hinitoccper,n_run,operation,value"
+        dataline="$cachesize,$recordcount,$fieldcount,$fieldlength,$operationcount,$threads,$binding,${workload}_${ycsb_job},$integrity,$distribution,$readonly,$preload,$memory,$oop,$filesystem,$heapsize,$hinitoccper,$n_run,$operation,$value"
 
         datafilepath="${DATADIR}/${datafile}"
 
